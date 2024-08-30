03694025face09dcfa12d353cf44e2acd9db3668
Anne <annaconda1984@yahoo.co.uk>
2024-08-30 12:22:53 +0100

    ready to deploy

--- flavourpalette/settings.py
+++ flavourpalette/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import django_on_heroku
+import dj_database_url
 from pathlib import Path
 import environ
 from datetime import timedelta
@@ -104,15 +105,23 @@ WSGI_APPLICATION = 'flavourpalette.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+   # 'default': {
+    #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       # 'NAME': env('DATABASE_NAME'),
+        #'USER': env('DATABASE_USER'),
+       # 'PASSWORD': env('DATABASE_PASS'),
+       # 'HOST': 'localhost',
+        #'PORT': 5432
+   # }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASS'),
-        'HOST': 'localhost',
-        'PORT': 5432
-    }
+    'default': dj_database_url.config(
+        default=env('DB_URI'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
@@ -161,3 +170,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals())
